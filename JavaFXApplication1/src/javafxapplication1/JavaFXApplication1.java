4156b94d6f1a1f9796b5e41e572409effd7c6546
Wellcome <Wellcome@DESKTOP-8REH2JD>
2026-01-23 19:56:14 +0600

    added login

--- JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
+++ JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
@@ -4,9 +4,12 @@
  */
 package javafxapplication1;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -19,25 +22,13 @@ import javafx.stage.Stage;
 public class JavaFXApplication1 extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) throws IOException {
+        Parent root= FXMLLoader.load(getClass().getResource("roleSelection.fxml"));
+          Scene scene= new Scene(root);
+          primaryStage.setScene(scene);
+          primaryStage.setTitle("SOSync");
+          
+          primaryStage.show();
     }
 
     /**
